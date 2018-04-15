6756d647e3ad4279f4c0c284fa5c03da17e3b557
Haseeb Nasee <haseebnaseem@gmail.com>
2018-04-15 23:55:39 +0500

    authentication

--- index.js
+++ index.js
@@ -2,11 +2,74 @@ const express = require('express');
 const app = express();
 const UserProvider = require('./userProvider');
 const userProvider = new UserProvider();
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+
+
+app.set('view engine','jade');
+app.use(require('morgan')('combined'));
+app.use(express.static('public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({secret:'perfect you', resave:false, saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+passport.use(new Strategy(
+    function(username,password,cb){
+        userProvider.findByUsername(username,password,function(err,user){
+            console.log('-----------');
+            console.log(user);
+            console.log('-----------');
+            if(err) {return cb(err);}
+            if(!user) {return cb(null,false);}
+            return cb(null,user);
+        });
+    }));
+
+passport.deserializeUser(function(id,cb){
+    userProvider.findById(id,function(err,user){
+        if(err) { return cb(err);}
+        cb(null,user);
+    });
+});
+
+passport.serializeUser(function(user,cb){
+    cb(null,user._id);
+});
+
 function health(req,res){
 	userProvider.getAll(function(docs){
 		console.log(docs);
 	});
 	res.status(200).send('service is ok');
 }
+
 app.get('/health',health);
-app.listen(3000,()=>console.log('listening at 3000'));
+app.get('/',(req,res)=>{
+    res.render('index',{user:req.user});
+});
+app.get('/log.html',(req,res)=>{
+    res.render('login');
+});
+app.get('/home',(req,res)=>{
+    res.render('home',{user:req.user});
+});
+app.post('/register',(req,res)=>{
+    userProvider.saveUser(req.body,function(err,user){
+        if(err)
+            res.status(300).send(err);
+        else
+            res.redirect('/');
+    });
+});
+app.post('/login',
+    passport.authenticate('local',{failiureRedirect: '/log.html'}),
+    function(req,res){
+        res.redirect('/home');
+    });
+app.listen(1598,()=>console.log('listening at 3000'));
--- userProvider.js
+++ userProvider.js
@@ -1,18 +1,16 @@
 const monk = require('monk');
 const bcrypt = require('bcrypt');
-
-const url = 'localhost:27017/users';
+const SaltRounds = 10;
+const url = 'mongodb://manahil:hashmi@ds231589.mlab.com:31589/users';
 var db = null;
 var UserProvider = function (){
 	db = monk(url,function(err,db){
-		console.log(err);
-		console.log(db);
+        console.log(err);
 	});
-		
 }
 
 UserProvider.prototype.getAll = function (callback){
-	collection = db.get('users');
+	collection = db.get('testUsers');
 	collection.find().then(docs=>{
 		callback(docs);
 	}).catch(err=>{
@@ -20,4 +18,52 @@ UserProvider.prototype.getAll = function (callback){
 	});
 }
 
+UserProvider.prototype.findByUsername = function (username,password,callback) {
+    collection = db.get('testUsers');
+    collection.findOne({username:username}).then(user=>{
+        bcrypt.compare(password,user.password,function(err,res){
+            if(err) {return callback(err)}
+            if(res)
+                callback(null,user);
+            else
+                callback(null,null);
+        });
+    }).catch(err=>{
+        console.log(err);
+        callback( new Error('User ' + username + ' does not exist'));
+    });
+}
+
+UserProvider.prototype.findById = function (id,callback){
+    collection = db.get('testUsers');
+    collection.findOne({_id:id}).then(user=>{
+        console.log(user);
+        callback(null,user);
+    }).catch(err=>{
+        console.log(err);
+        callback( new Error('User ' + id + ' does not exist'));
+    });
+}
+
+UserProvider.prototype.saveUser = function (user,callback){
+    console.log(user);
+    collection = db.get('testUsers');
+    collection.findOne({username:user.username}).then(thisUser=>{
+        if(thisUser)
+            callback(new Error('user already exists'));
+        else{
+            bcrypt.hash(user.password,SaltRounds,function(err,hash){
+                if(err) { return callback(err) }
+                user.password = hash;
+                collection.insert(user).then((user)=>{
+                    callback(null,user);
+                }).catch(err=>{
+                    callback(err);
+                });
+            });
+        }
+    }).catch(err=>{
+        callback(new Error(JSON.stringify(err)));
+    });
+}
 module.exports = UserProvider;
