bf8b4aeaebf5b1e9d10fd35ae953f52ea606d366
Manahil <manahilhashmi@gmail.com>
2018-04-16 00:04:26 +0500

    New version

---
+++ public/js/index.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+    console.log('here')
+    $('.counter').each(function() {
+      var $this = $(this),
+          countTo = $this.attr('data-count');
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
+      },
+
+      {
+
+        duration: 10000,
+        easing:'linear',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+          //alert('finished');
+        }
+
+      });  
+    });
+    $(function(){
+    $('#logbutton').click(function(){
+        window.location='log.html'
+    });
+    });
+     $(function(){
+    $('#bannerButton').click(function(){
+        window.location='log.html'
+    });
+    });
+    $(function(){
+    $('#signBt').click(function(){
+        window.location='log.html'
+    });
+    });
+    
+  function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.chart span');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
+
+})
+ 
